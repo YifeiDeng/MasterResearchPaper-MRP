d3984e25f3978467bd97d56ff1ec6a40ab52080f
YifeiDeng <33532974+YifeiDeng@users.noreply.github.com>
2021-01-14 10:45:35 -0500

    Update Figure_6_1.m

--- Figure_6_1.m
+++ Figure_6_1.m
@@ -21,7 +21,7 @@ for i = 1:size(run,2)
     [V_rQMC(i),se_rQMC(i)] = Lookback_Option_ADGBS_RQMC(M, run_rQMC(i), sobol_num, epsilon);
 end
 
-% Gives the plot
+% Gives the plot(MC)
 figure(1)
 plot(run, se, 'b-')
 xlim([min(run)-10^4, max(run)+10^4])
@@ -29,11 +29,43 @@ xlabel('Total Simulation Runs')
 ylabel('Standard Error')
 title('Monte-Carlo Method')
 
+% log-log scale
 figure(2)
+plot(log(run), log(se), 'b-')
+xlim([min(run)-10^4, max(run)+10^4])
+xlabel('log(Total Simulation Runs)')
+ylabel('log(Standard Error)')
+title('Monte-Carlo Method (log-log scale)')
+axis auto
+
+% Gives the plot(rQMC)
+figure(3)
 plot(run, se_rQMC, 'r-')
 xlim([min(run)-10^4, max(run)+10^4])
 xlabel('Total Simulation Runs')
 ylabel('Standard Error')
 title('Randomized Quasi-Monte-Carlo Method')
 
+% log-log scale
+figure(4)
+plot(log(run), log(se_rQMC), 'r-')
+xlim([min(run)-10^4, max(run)+10^4])
+xlabel('log(Total Simulation Runs)')
+ylabel('log(Standard Error)')
+title('Randomized Quasi-Monte-Carlo Method (log-log scale)')
+axis auto
 
+% Conbined the plot
+coef = polyfit(log(run), log(se_rQMC)',1);
+coef_mc = polyfit(log(run), log(se)',1);
+hold on
+plot(log(run), log(se), 'b-')
+plot(log(run), log(run)*coef_mc(1)+coef_mc(2), 'b--')
+plot(log(run), log(se_rQMC), 'r-')
+plot(log(run), log(run)*coef(1)+coef(2), 'r--')
+hold off
+xlabel('log(Total Simulation Runs)')
+ylabel('log(Standard Error)')
+title('MC and rQMC (log-log scale)')
+axis auto
+legend('MC','Linear Fit for MC: y = -0.4991x+1.9566','rQMC','Linear Fit for rQMC: y = -0.6923x+2.2966', 'FontSize',13)
